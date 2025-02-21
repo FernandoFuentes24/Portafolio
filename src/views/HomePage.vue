f1ca688b50a3ec05604aa00179a5bfa5fdbaae19
FernandoFuentes24 <fernandoj2526@gmail.com>
2025-02-21 10:24:53 -0600

    estiloHomepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,43 @@
 <template>
-    <header class="text-center p-5 bg-primary text-white">
+   <div class="home">
       <h1>Fernando Fuentes</h1>
-      <p>Desarrollador Web | Vue.js | Bootstrap</p>
-    </header>
-  </template>
-  
+      <h2>Junior Web Developer</h2>
+      <p>LARAVEL | .NET CORE | .NET MAUI | MYSQL | BOOTSTRAP | VUE JS </p>
+      <button type="button" class="btn btn-secondary">Proyectos</button>
+      <button type="button" class="btn btn-secondary">Contacto</button>
+  </div>
+</template>
+
+<style scoped>
+
+.home {
+  text-align: center;
+  padding: 2rem;
+  height: 90vh;
+  width: 100%;
+  background-image: url('../assets/image/stars.svg');
+  background-color: black;
+}
+
+.home h1 {
+  font-size: 4rem;
+  font-family: 'Roboto', sans-serif;
+  color: #887e20;
+}
+
+.home h2 {
+  font-size: 2rem;
+ 
+  font-family: 'Roboto', sans-serif;
+  color: #ffffff;
+}
+
+.home p {
+  font-size: 1.2rem;
+  font-family: 'Roboto', sans-serif;
+  color: #ffffff;
+}
+
+
+
+</style>
