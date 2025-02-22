12caa90ec6d20eb95d0584622ec354ced106e1de
mauri <mauri@DESKTOP-C5H1MHC>
2025-02-22 16:59:10 -0600

    Logos

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,26 +3,42 @@
       <h1>Fernando Fuentes</h1>
       <h2>Junior Web Developer</h2>
       <p>LARAVEL | .NET CORE | .NET MAUI | MYSQL | BOOTSTRAP | VUE JS </p>
+    
+    <!--Logos-->
+    <div class="logos">
+      <img src="../assets/image/Linkedln.png" alt="Logo 1" class="logo">
+      <img src="../assets/image/Github.png" alt="Logo 2" class="logo">
+    </div>
+    
+      <hr color="white" size="8" width="290"><br>
+
+      <!--Botones-->
+      <div class="buttons-container">
       <button type="button" class="btn btn-secondary">Proyectos</button>
       <button type="button" class="btn btn-secondary">Contacto</button>
   </div>
+</div>
 </template>
 
 <style scoped>
 
 .home {
+  display:  flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  padding: 2rem;
+  padding: 5rem;
   height: 90vh;
   width: 100%;
   background-image: url('../assets/image/stars.svg');
-  background-color: yellow;
+  background-color: rgb(0, 0, 0);
 }
 
 .home h1 {
   font-size: 4rem;
   font-family: 'Roboto', sans-serif;
-  color: #887e20;
+  color: #3ff3f3;
 }
 
 .home h2 {
@@ -37,7 +53,19 @@
   font-family: 'Roboto', sans-serif;
   color: #ffffff;
 }
-
-
-
+.buttons-container{
+  display: flex;
+  gap: 1rem;
+}
+ .logo{
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+ }
+ .buttons-container{
+  display: flex;
+  gap: 1rem;
+  margin-top: 1rem;
+ }
 </style>
